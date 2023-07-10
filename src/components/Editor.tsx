68f85b0e9c7fb621d8219be12d6e085bb2f00adc
isaiasg09 <isaiashsobral@gmail.com>
2023-07-10 20:11:48 -0300

    added float menu and placeholder

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,11 @@
-import { BubbleMenu, EditorContent, useEditor } from "@tiptap/react";
+import '../index.css';
+
+import {
+  BubbleMenu,
+  EditorContent,
+  useEditor,
+  FloatingMenu,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { initialContent } from "./initialContent";
 // import BubbleMenu from "@tiptap/extension-bubble-menu";
@@ -22,6 +29,8 @@ import {
   RxUnderline,
 } from "react-icons/rx";
 import { BubbleButton } from "./BubbleButton";
+
+import Placeholder from "@tiptap/extension-placeholder";
 import Underline from "@tiptap/extension-underline";
 
 lowlight.registerLanguage("html", html);
@@ -40,6 +49,9 @@ export function Editor() {
       // BubbleMenu.configure({
       //   element: document.querySelector(".menu"),
       // }),
+      Placeholder.configure({
+        placeholder: "Press '/' for commands...",
+      }),
     ],
     content: initialContent,
     editorProps: {
@@ -51,21 +63,48 @@ export function Editor() {
 
   return (
     <>
+      {/* Floating Menu */}
+      {editor && (
+        <FloatingMenu
+          editor={editor}
+          shouldShow={({ state }) => {
+            const { $from } = state.selection;
+
+            const currentLineText = $from.nodeBefore?.textContent;
+
+            return currentLineText === "/";
+          }}
+          className="bg-zinc-700 py-2 px-1 shadow-xl border border-zinc-600 shadow-black/20 rounded-md overflow-hidden flex flex-col transition"
+        >
+          <button className="flex items-center gap-2 py-1 px-2 rounded min-2-[280px] hover:bg-zinc-600 transition">
+            <img
+              src="http://www.notion.so/images/blocks/text/en-US.png"
+              alt="Text"
+              className="w-12 border border-zinc-600 rounded"
+            />
+
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xs text-zinc-400">
+                Just start writing with plain text.
+              </span>
+            </div>
+          </button>
+        </FloatingMenu>
+      )}
+
+      {/* Bubble Menu */}
       {editor && (
         <BubbleMenu
           className="bg-zinc-700 shadow-xl  shadow-black/20 rounded-md overflow-hidden flex divide-x divide-zinc-600 transition"
           editor={editor}
           tippyOptions={{ duration: 100 }}
         >
-          <BubbleButton
-            onClick={() => editor.chain().focus().toggleCode().run()}
-          >
+          <BubbleButton>
             Text
             <RxChevronDown className="h-4 w-4 " />
           </BubbleButton>
-          <BubbleButton
-            onClick={() => editor.chain().focus().toggleCode().run()}
-          >
+          <BubbleButton>
             <RxChatBubble className="h-4 w-4 " />
             Comment
           </BubbleButton>
